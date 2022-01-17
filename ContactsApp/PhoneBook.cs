c402e6e3f5c90de86b4efd3e47c0df801c16b5a9
Konrad Kruczkowski <Konrad.Kruczkowski@consolis.com>
2022-01-18 00:17:04 +0100

    To 10 minutes

---
+++ ContactsApp/PhoneBook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; }
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Number == number);
+
+            if(contact == null)
+                Console.WriteLine("Contacts not found");
+            else
+            {
+                DisplayContactsDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            FunctionForeach(Contacts);
+        }
+
+        private void DisplayContactsDetails(Contact contact )
+        {
+            Console.WriteLine($"Contact: { contact.Name}, {contact.Number}");
+        }
+
+        private void FunctionForeach(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactsDetails(contact);
+            }
+        }
+
+
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContacts = Contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            FunctionForeach(matchingContacts);
+        }
+    }
+}
--- ContactsApp/Program.cs
+++ ContactsApp/Program.cs
@@ -6,7 +6,7 @@ namespace ContactsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from the PhoneBook app");
         }
     }
 }
